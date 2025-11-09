c226175889968c579b7aee86422dc5c52eb71a9e
user.shadow:"dishantyadav84gmail.com <dishantyadav84@gmail.com>
2025-11-09 12:20:28 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
  import { configDotenv } from "dotenv";
 import express from "express"
+import cors from  "cors";
 import connectDB from "./db/db.js";
 import Words from "./routes/get.js";
 import Kanji from "./routes/getKanji.js";
 import Story from "./routes/story.js";
  configDotenv();
  const app = express();
+
   app.use(express.json());
+  app.use(cors({
+    origin : "*",
+    methods:"GET"
+  }));
 
  app.use("/v1/words",Words);
  app.use("/v1/kanji",Kanji);
